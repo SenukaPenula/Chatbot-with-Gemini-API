d45ff8dae51e2bd6476a2b099586717b11c702c9
Senuka Penula <167842769+SenukaPenula@users.noreply.github.com>
2025-01-02 20:36:11 +0530

    Update script.js
    Apil

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let isResponseGenerating = false;
 
 
 // Api config
-const GEMINI_API_KEY = "AIzaSyAwjfQOhbzckJ8mSZjCFwjMul9nxkaNtgE";
+const GEMINI_API_KEY = "AIzaSyAuoynCiROhm9qdvFsUS0mLQcgzUnQN7CI";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 const loadLocalStorageData = () => {
